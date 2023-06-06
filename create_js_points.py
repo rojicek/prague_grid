1b5f777ff3d6dde9ea2f5c6c20642a32b57794ac
Jiri Rojicek <jiri@rojicek.cz>
2023-06-06 21:15:31 +0200

    wip

--- create_js_points.py
+++ create_js_points.py
@@ -10,8 +10,15 @@ import pandas as pd
 root_folder = os.path.join(pathlib.Path(__file__).parent.resolve(), 'output')
 
 square_distance = 1000
+reduced = True  # vyber jestli chces orig nebo reduced!!
 
-file_name_hdf = os.path.join(root_folder, f'grid_{str(square_distance).zfill(5)}.hdf')
+
+if reduced:
+    file_name_hdf = os.path.join(root_folder, f'grid_reduced_{str(square_distance).zfill(5)}.hdf')
+else:
+    file_name_hdf = os.path.join(root_folder, f'grid_{str(square_distance).zfill(5)}.hdf')
+
+
 file_name_dict = os.path.join(root_folder, f'dict_{str(square_distance).zfill(5)}.json')
 
 df_points = pd.read_hdf(file_name_hdf)
@@ -29,6 +36,7 @@ if len(np.unique(df_points[['ix', 'iy']], axis=0)) != len(df_points):
 if (max_ix+1) * (max_iy+1) != len(df_points):
     print('NESEDI POCET BODU!!')
 
+
 for ix in range(0, max_ix+1):
     for iy in range(0, max_iy + 1):
         one_point = df_points[(df_points['ix']==ix) & (df_points['iy']==iy)]
@@ -36,6 +44,7 @@ for ix in range(0, max_ix+1):
         if one_point['state'].iloc[0].lower() == 'praha':
             d[id] = f"{one_point['latitude'].iloc[0]}N,{one_point['longitude'].iloc[0]}E"
             id = id + 1
+
 
 with open(file_name_dict, 'w') as f:
     f.write(str(d))
--- grid_step2.py
+++ grid_step2.py
@@ -15,7 +15,14 @@ square_distance = 1000
 
 file_name_hdf = os.path.join(root_folder, f'grid_{str(square_distance).zfill(5)}.hdf')
 
+
+file_namereduced_hdf = os.path.join(root_folder, f'grid_reduced_{str(square_distance).zfill(5)}.hdf')
+file_namereduced_csv = os.path.join(root_folder, f'grid_reduced_{str(square_distance).zfill(5)}.csv')
+
 df_points = pd.read_hdf(file_name_hdf)
+# kopie kterou budu prepisovat
+df_points_reduced  = df_points.copy()
+
 max_ix = df_points['ix'].max()
 max_iy = df_points['iy'].max()
 state = 'praha'
@@ -29,17 +36,47 @@ if (max_ix+1) * (max_iy+1) != len(df_points):
 
 for ix in range(0, max_ix+1):
     for iy in range(0, max_iy + 1):
-        print (ix)
-        print (iy)
-        print ('----')
         # u sw rohu nemusim testovat, jestli je v df - urcite je
-        sw_in = (df_points[(df_points['ix'] == ix) & (df_points['iy'] == iy)]['state'].iloc[0].lower() == 'state')
+        sw_in = (df_points[(df_points['ix'] == ix) & (df_points['iy'] == iy)]['state'].iloc[0].lower() == state)
 
         # se
         if ix+1 <= max_ix:
-            se_in = (df_points[(df_points['ix'] == ix+1) & (df_points['iy'] == iy)]['state'].iloc[0].lower() == 'state')
+            se_in = (df_points[(df_points['ix'] == ix+1) & (df_points['iy'] == iy)]['state'].iloc[0].lower() == state)
         else:
             se_in = False # ani neni v dataframe
 
-        xxx = 0
+        if iy + 1 <= max_iy:
+            nw_in = (df_points[(df_points['ix'] == ix) & (df_points['iy'] == iy+1)]['state'].iloc[0].lower() == state)
+        else:
+            nw_in = False  # ani neni v dataframe
+
+        if (iy + 1 <= max_iy) and (ix+1 <= max_ix):
+            ne_in = (df_points[(df_points['ix'] == ix+1) & (df_points['iy'] == iy+1)]['state'].iloc[0].lower() == state)
+        else:
+            ne_in = False  # ani neni v dataframe
+
+        all_in = sw_in and se_in and nw_in and ne_in
+
+        if not all_in:
+            ix_replace = df_points_reduced[(df_points_reduced['ix'] == ix) & (df_points_reduced['iy'] == iy)].index
+            df_points_reduced.loc[ix_replace, 'state'] = 'smazano'
+        else:
+            rrr = 0
+            # nic
+
+df_points_reduced.reset_index(inplace=True, drop=True)
+df_points_reduced.to_csv(file_namereduced_csv)
+df_points_reduced.to_hdf(file_namereduced_hdf, key='data')
+
+praha_orig = len(df_points[df_points['state'].str.lower() == state])
+praha_reduced = len(df_points_reduced[df_points_reduced['state'].str.lower() == state])
+
+print(f'zredukovano z {praha_orig} na {praha_reduced}')
+print('konec')
+
+
+
+
+
+
 
