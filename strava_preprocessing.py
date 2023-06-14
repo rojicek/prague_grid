1b269b6c2ccb9ff5dd89422141430884fe87acff
Jiri Rojicek <jiri@rojicek.cz>
2023-06-14 22:33:11 +0200

    wip

--- strava_preprocessing.py
+++ strava_preprocessing.py
@@ -10,6 +10,7 @@ from geopy.distance import geodesic
 from fit2gpx import Converter
 from fit2gpx import StravaConverter
 import os
+from tcx2gpx import TCX2GPX
 
 import pandas as pd
 
@@ -17,24 +18,33 @@ import pandas as pd
 
 fit_folder_path = r'c:\Users\jiri\Documents\dev_code\Prague_grid\prague_grid\rides'
 
+# tcx input files
+tcx_folder_path = os.path.join(fit_folder_path, 'activities')
 # output folder for gpx files
 gpx_folder_path = os.path.join(fit_folder_path, 'activities_gpx')
 
+if False:
+    strava_conv = StravaConverter(dir_in=fit_folder_path, dir_out=gpx_folder_path)
 
-strava_conv = StravaConverter(dir_in=fit_folder_path, dir_out=gpx_folder_path)
+    # Step 2: Unzip the zipped files
+    strava_conv.unzip_activities()
 
-# Step 2: Unzip the zipped files
-strava_conv.unzip_activities()
+    # Step 3: Add metadata to existing GPX files and copy gpx files
+    strava_conv.add_metadata_to_gpx()
 
-# Step 3: Add metadata to existing GPX files and copy gpx files
-strava_conv.add_metadata_to_gpx()
-
-print('start actual conversion')
-# Step 4: Convert FIT to GPX
-strava_conv.strava_fit_to_gpx()
+    print('start actual conversion')
+    # Step 4: Convert FIT to GPX
+    strava_conv.strava_fit_to_gpx()
 
 #todo: chybi tcx soubory z 2013/2014 ... chybi Rakousko, Lipot
 # v Praze to ale zadny novy ctverec nebude
 # 65 tcx souboru
+
+for files in os.listdir(tcx_folder_path):
+    if files.endswith('.tcx'):
+        print(files)  # printing file name of desired extension
+    else:
+        continue
+
 
 print('konec')
